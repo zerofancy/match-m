0f6533ac6901629e6a6c467090687cfe608ca4ed
zerofancy <ntutn.top@gmail.com>
2021-08-22 07:36:04 +0800

    安卓跑起来了

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -29,24 +29,24 @@ kotlin {
                 api(compose.material)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
+//        val commonTest by getting {
+//            dependencies {
+//                implementation(kotlin("test"))
+//            }
+//        }
         val androidMain by getting {
             dependencies {
                 api("androidx.appcompat:appcompat:1.2.0")
                 api("androidx.core:core-ktx:1.3.1")
             }
         }
-        val androidTest by getting {
-            dependencies {
-                implementation("junit:junit:4.13")
-            }
-        }
+//        val androidTest by getting {
+//            dependencies {
+//                implementation("junit:junit:4.13")
+//            }
+//        }
         val desktopMain by getting
-        val desktopTest by getting
+//        val desktopTest by getting
     }
 }
 
