0a9202b457cd4dd0a9122ef6298fab2d78a5649d
zerofancy <ntutn.top@gmail.com>
2021-08-23 04:23:27 +0800

    code format

--- common/src/commonMain/kotlin/top/ntutn/common/App.kt
+++ common/src/commonMain/kotlin/top/ntutn/common/App.kt
@@ -1,19 +1,16 @@
 package top.ntutn.common
+
 import androidx.compose.foundation.Image
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 
 @Composable
-fun App(resourceItemCount:Int, getPainterById:@Composable (Int)->Painter) {
-    var text by remember { mutableStateOf("Hello, World!") }
-
+fun App(resourceItemCount: Int, getPainterById: @Composable (Int) -> Painter) {
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, ${getPlatformName()}"
-        }) {
-            Image(painter = getPainterById((0 until resourceItemCount).random()),contentDescription = null)
-        }
+        Image(
+            painter = getPainterById((0 until resourceItemCount).random()),
+            contentDescription = null
+        )
     }
 }
