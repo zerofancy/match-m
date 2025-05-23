09a4785dfb56c3c8857cc2114bd534245f191205
zerofancy <ntutn.top@gmail.com>
2025-05-24 02:16:01 +0800

    解决web无法加载问题

--- composeApp/src/commonMain/kotlin/top/ntutn/match/ui/SettingScreen.kt
+++ composeApp/src/commonMain/kotlin/top/ntutn/match/ui/SettingScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,6 +19,12 @@ import zmatch.composeapp.generated.resources.Res
 import zmatch.composeapp.generated.resources.setting_screen_exit
 import zmatch.composeapp.generated.resources.setting_screen_options
 import zmatch.composeapp.generated.resources.setting_screen_title
+
+private val optionArray = arrayOf( // todo web字符串列表资源处理有问题，需要报告一下
+    "简单",
+    "普通",
+    "困难"
+)
 
 @Composable
 fun SettingScreen(
@@ -31,7 +38,8 @@ fun SettingScreen(
         Difficulty.entries.forEachIndexed { index, item ->
             Row(verticalAlignment = Alignment.CenterVertically) {
                 RadioButton(selected = difficulty == item, onClick = { onDifficultyChange(item) })
-                Text(stringArrayResource(Res.array.setting_screen_options)[index])
+//                Text(stringArrayResource(Res.array.setting_screen_options)[index])
+                Text(optionArray[index])
             }
         }
         Spacer(modifier = Modifier.height(32.dp))
